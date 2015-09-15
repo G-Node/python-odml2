29bfaf29045488e0dc48dba53b3283d74bb9ed6c
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2015-09-15 13:23:46 +0200

    compat.py: some simple compatibility helpers

---
+++ odml2/compat.py
@@ -0,0 +1,32 @@
+# coding=UTF-8
+
+# Copyright (c) 2015, Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+"""
+Some utilities for python 2 and 3 compliant code
+"""
+
+import sys
+import abc
+
+__all__ = ("PY2", "PY3", "ABC", "unicode")
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY2:
+    class ABC(object):
+        __metaclass__ = abc.ABCMeta
+else:
+    from abc import ABC
+
+if PY2:
+    unicode = unicode
+else:
+    unicode = str
