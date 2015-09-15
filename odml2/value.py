01a22192650fa295dec903236055259509cf3c57
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2015-09-15 13:25:50 +0200

    value.py: basic immutable Value class with tests
    So far the class does not check for allowed values for `value`, `unit` and `uncertainty`.

---
+++ odml2/value.py
@@ -0,0 +1,83 @@
+# coding=UTF-8
+
+# Copyright (c) 2015, Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+import datetime as dt
+import odml2.compat as compat
+
+__all__ = ("Value", )
+
+PLUS_MINUS_UNICODE = u"±"
+PLUS_MINUS = PLUS_MINUS_UNICODE if compat.PY3 else "+-"
+
+
+class Value(object):
+    """
+    An odML Value class
+    """
+
+    def __init__(self, value, unit=None, uncertainty=None):
+        self.__value = value
+        self.__unit = unit
+        self.__uncertainty = uncertainty
+
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def unit(self):
+        return self.__unit
+
+    @property
+    def uncertainty(self):
+        return self.__uncertainty
+
+    def set(self, value=None, unit=None, uncertainty=None):
+        return Value(
+            value if value is not None else self.value,
+            unit if unit is not None else self.unit,
+            uncertainty if uncertainty is not None else self.uncertainty
+        )
+
+    def __eq__(self, other):
+        if isinstance(other, Value):
+            return self.value == other.value and self.unit == other.unit and self.uncertainty == other.uncertainty
+        else:
+            return False
+
+    @property
+    def __value_str(self):
+        if isinstance(self.value, (dt.date, dt.time, dt.datetime)):
+            return self.value.isoformat()
+        elif isinstance(self.value, (compat.unicode, str)):
+            return self.value
+        else:
+            return str(self.value)
+
+    def __str__(self):
+        parts = [self.__value_str]
+        if self.unit is not None:
+            parts.append(self.unit)
+        if self.uncertainty is not None:
+            parts.append(PLUS_MINUS)
+            parts.append(str(self.uncertainty))
+        return str().join(parts)
+
+    def __unicode__(self):
+        parts = [self.__value_str]
+        if self.unit is not None:
+            parts.append(self.unit)
+        if self.uncertainty is not None:
+            parts.append(PLUS_MINUS_UNICODE)
+            parts.append(str(self.uncertainty))
+        return compat.unicode().join(parts)
+
+    def __repr__(self):
+        return str(self)
---
+++ test/value_test.py
@@ -0,0 +1,71 @@
+# coding=UTF-8
+
+# Copyright (c) 2015, Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+import nose
+import unittest
+
+import odml2.compat as compat
+from odml2.value import Value
+
+
+class ValueTest(unittest.TestCase):
+
+    def test_init(self):
+        v1 = Value("hello")
+        self.assertEqual(v1.value, "hello")
+        self.assertIsNone(v1.unit)
+        self.assertIsNone(v1.uncertainty)
+
+        v2 = Value(100, "mV", 0.001)
+        self.assertEqual(v2.value, 100)
+        self.assertEqual(v2.unit, "mV")
+        self.assertEqual(v2.uncertainty, 0.001)
+
+    def test_set(self):
+        v1 = Value(0)
+        self.assertEqual(v1.value, 0)
+        self.assertIsNone(v1.unit)
+        self.assertIsNone(v1.uncertainty)
+
+        v1 = v1.set(value=100)
+        self.assertEqual(v1.value, 100)
+        self.assertIsNone(v1.unit)
+        self.assertIsNone(v1.uncertainty)
+
+        v1 = v1.set(unit="mV")
+        self.assertEqual(v1.value, 100)
+        self.assertEqual(v1.unit, "mV")
+        self.assertIsNone(v1.uncertainty)
+
+        v1 = v1.set(uncertainty=0.1)
+        self.assertEqual(v1.value, 100)
+        self.assertEqual(v1.unit, "mV")
+        self.assertEqual(v1.uncertainty, 0.1)
+
+    def test_eq(self):
+        self.assertEqual(Value("foo"), Value("foo"))
+        self.assertEqual(Value(10, "mV"), Value(10.0, "mV"))
+        self.assertEqual(Value(1, "ms", 0.11), Value(1, "ms", 0.11))
+
+        self.assertNotEqual(Value("foo"), Value("bar"))
+        self.assertNotEqual(Value(10, "mV"), Value(10.1, "mV"))
+        self.assertNotEqual(Value(1, "ms", 0.11), Value(1, "ms", 0.0))
+
+    def test_str(self):
+        v1 = Value(1, "mV", 0.1)
+
+        if compat.PY2:
+            self.assertEqual(str(v1), "1mV+-0.1")
+            self.assertEqual(compat.unicode(v1), u"1mV±0.1")
+        else:
+            self.assertEqual(str(v1), "1mV±0.1")
+
+if __name__ == '__main__':
+    nose.main()
