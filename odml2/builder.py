360c927c38199ac531700fcdf158359f9819a348
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2015-10-01 16:21:12 +0200

    builder.py: module for builders
    with very basic draft for a section builder

---
+++ odml2/builder.py
@@ -0,0 +1,23 @@
+# coding=UTF-8
+
+# Copyright (c) 2015, Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+__all__ = ("SB", )
+
+
+class SB(object):
+    """
+    A section builder
+    """
+
+    def __init__(self, typ, uuid=None, label=None, references=None, **properties):
+        pass
+
+    def build(self, back_end, parent):
+        pass
