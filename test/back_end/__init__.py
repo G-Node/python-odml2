cb61f79be0829dd299b04f0b73839ad56d507d2d
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2016-01-29 16:16:19 +0100

    Remove old back-end

--- test/back_end/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding=UTF-8
-
-# Copyright (c) 2015, Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de)
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted under the terms of the BSD License. See
-# LICENSE file in the root of the project.
